93cf92fea19117c7f3c4947d74e885c174901241
TomMartinez15 <tommaldonado4@gmail.com>
2025-07-30 14:35:13 -0300

    Limite alteradod e 10 para 100.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumero();
 let tentativas = 1;
 
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10'); 
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100'); 
 }
 
 exibirMensagemInicial();
